9fd84260ae3ce71f4d6231affc95d07899f8197e
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-03 19:15:47 +0200

    ajout de la fonction signuppour la création d'un utilisateur

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const bookRoutes = require("./routes/book");
+const userRoutes = require("./routes/user");
 
 const app = express();
 
@@ -34,5 +35,6 @@ app.use((req, res, next) => {
 });
 
 app.use("/api/book", bookRoutes);
+app.use("/api/auth", userRoutes);
 
 module.exports = app;

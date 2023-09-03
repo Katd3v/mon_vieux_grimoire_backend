e92c4e858f9e7520d339b03e569636fe8fbe16be
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-03 15:33:54 +0200

    ajout de modèle thing + création route pour lire, ecrire et modifier un objet

--- app.js
+++ app.js
@@ -1,9 +1,65 @@
 const express = require("express");
+const mongoose = require("mongoose");
+
+const Thing = require("./models/Thing");
 
 const app = express();
 
-app.use((req, res) => {
-  res.json({ message: "un message" });
+mongoose
+  .connect(
+    "mongodb+srv://katleen:54hLyRN54tunn4l3@katd3v.cz5m9fl.mongodb.net/?retryWrites=true&w=majority",
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    }
+  )
+  .then(() => console.log("Connexion à MongoDB réussie !"))
+  .catch(() => console.log("Connexion à MongoDB échouée !"));
+
+// Extraction du corps JSON afin de gérer la requête POST venant de l'application front-end
+app.use(express.json());
+
+// Gestion des erreurs CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
+
+app.post("/api/books", (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body,
+  });
+  thing
+    .save()
+    .then(() => res.status(201).json({ message: "Objet enregistré !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.put("/api/books/:id", (req, res, next) => {
+  Thing.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: "Objet modifié !" }))
+    .catch((error) => res.status(400).json({ error }));
+});
+
+app.get("/api/books/:id", (req, res, next) => {
+  Thing.findOne({ _id: req.params.id })
+    .then((thing) => res.status(200).json(thing))
+    .catch((error) => res.status(404).json({ error }));
+});
+
+app.get("/api/books", (req, res, next) => {
+  Thing.find()
+    .then((things) => res.status(200).json(things))
+    .catch((error) => res.status(400).json({ error }));
 });
 
 module.exports = app;

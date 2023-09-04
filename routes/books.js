bf244a8738372c78e79b7ea5f0f6cf16fced214d
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-04 17:25:59 +0200

    modification de la route put

--- routes/books.js
+++ routes/books.js
@@ -7,7 +7,7 @@ const bookCtrl = require("../controllers/books");
 router.get("/", bookCtrl.getAllBooks);
 router.post("/", auth, multer, bookCtrl.createBook);
 router.get("/:id", bookCtrl.getOneBook);
-router.put("/:id", auth, bookCtrl.modifyBook);
+router.put("/:id", auth, multer, bookCtrl.modifyBook);
 router.delete("/:id", auth, bookCtrl.deleteBook);
 
 module.exports = router;
